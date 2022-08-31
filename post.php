8fd755d43400938f1b2f681ba7645f8c26b3693c
jirachanon <s6303051623098@email.kmutnb.ac.th>
2022-08-31 11:03:02 +0700

    8/31/2022

---
+++ post.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <h1 align = "center" >Webboard KodInw</h1>
+    
+            <center><?php echo "ต้องการดูกระทู้หมายเลข $_GET[id] "?></center><br>
+
+        <div align = "center">
+            <table style="border: 2px solid black; width: 40px;" align="center">
+                <tr><td style="background-color: #6cd2fe;" colspan="2">แสดงความคิดเห็น</td></tr>
+                <tr><td><textarea name="comment" id="" cols="30" rows="5"></textarea></td></tr>
+                <tr><td colspan="2" align="center"><input type="submit" value="ส่งความคิดเห็น"></td></tr>
+            </table> <br>
+            <a href="index.html">กลับไปหน้าหลัก</a>
+        </div>
+
+</body>
+</html>
